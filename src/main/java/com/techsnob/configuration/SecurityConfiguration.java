d38c100efc561e0fb8a31bd771089b28a23d55f5
vnareshkumar111 <vnareshkumar111@gmail.com>
2019-10-18 23:10:24 +0530

    Added some UI to login page, chnaged some configuration to resources

--- src/main/java/com/techsnob/configuration/SecurityConfiguration.java
+++ src/main/java/com/techsnob/configuration/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,20 +50,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	   http.csrf().disable().authorizeRequests()
 	  .antMatchers("/resources/**").permitAll()
 	  .antMatchers("/registration*").permitAll()
-	  .antMatchers("/h2-console*").permitAll()
+	  .antMatchers("/h2-console/**").permitAll()
 	  .antMatchers("/admin/**").hasRole("ADMIN")
 	  .antMatchers("/login*").permitAll()
 	  .anyRequest().authenticated()
 	  .and()
 	  .formLogin()
-	  .loginPage("/login.html")
+	  .loginPage("/login.html").permitAll(true)
 	  .defaultSuccessUrl("/index.html",false)
 	  //.failureUrl("/login.html?error=true")
 	  //.failureHandler(authenticationFailureHandler()) 
 	  .and()
 	  .logout()
 	  .logoutUrl("/logout") .deleteCookies("JSESSIONID");
+	   
+	   http.headers().frameOptions().disable();
+
 	}
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+      web
+        .ignoring()
+           .antMatchers("/resources/**").antMatchers("/plugins/**").antMatchers("/css/**").antMatchers("/js/**");
+    }
 
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
