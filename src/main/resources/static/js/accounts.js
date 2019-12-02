e74a3e70eab85ec928c9f1aad6b84443848330c8
vnareshkumar111 <vnareshkumar111@gmail.com>
2019-12-02 22:03:21 +0530

    Working row update after edit for accounts

--- src/main/resources/static/js/accounts.js
+++ src/main/resources/static/js/accounts.js
@@ -1,33 +1,47 @@
+var itemToUpdate = {};
+
 function saveAccount(isNew) {
-    $("#accountsGrid").jsGrid(isNew ? "insertItem" : "updateItem", $("#account").serializeJSON());
+    var accountsGrid = $("#accountsGrid");
+    if(isNew){
+        accountsGrid.jsGrid("insertItem", $("#account").serializeJSON());
+    } else {
+        accountsGrid.jsGrid("updateItem", itemToUpdate, $("#account").serializeJSON());
+    }
+    //itemToUpdate = {}; // Uncomment once finalizing the functionality
     $("#accountDialog").modal("hide");
 }
 
 function openAccountModal(mode, item){
-	if(mode == 'Edit'){
+    var actDlg = $("#accountDialog");
+    var addAct = $("#addAccount");
+    var vehicleNumber = $('#vehicleNumber');
+	if(mode === 'Edit'){
 		$('input[name="accountId"]').val(item.accountId);
 		$('input[name="accountHolderName"]').val(item.accountHolderName);
 		$('input[name="accountNumber"]').val(item.accountNumber);
 		$('input[name="ifscCode"]').val(item.ifscCode);
-        $('#vehicleNumber').append('<option value="'+item.vehicleNumber+'" selected>'+item.vehicleNumber+'</option');
-		$("#addAccount").attr("onclick", "saveAccount(false);");
-		$("#accountDialog").find('.modal-title').text("Edit Account");
+        vehicleNumber.html("").append('<option value="'+item.vehicleNumber+'" selected>'+item.vehicleNumber+'</option');
+        itemToUpdate = item;
+        addAct.attr("onclick", "saveAccount(false)");
+        actDlg.find('.modal-title').text("Edit Account");
 	} else {
+        $("#account")[0].reset();
 		ajaxGet('vehicleRCNumbers').success(function(response){
-			$("#vehicleNumber").html("").append('<option value="-1" disabled selected>--Select--</option>');
+            vehicleNumber.html("").append('<option value="-1" disabled selected>--Select--</option>');
 			$.each(response, function(i, val){
-				$("#vehicleNumber").append('<option value="'+val.rcNumber+'">'+val.rcNumber+'</option');
+                vehicleNumber.append('<option value="'+val.rcNumber+'">'+val.rcNumber+'</option');
 			});
 		});
-		$("#addAccount").attr("onclick", "saveAccount(true);");
+        actDlg.find('.modal-title').text("Add Account");
+        addAct.attr("onclick", "saveAccount(true);");
 	}
-	$("#accountDialog").modal('show');
+    actDlg.modal('show');
 }
 
 function initAccounts() {
     $("#accountsGrid").jsGrid({
         width: "100%",
-        height: "100%",
+        height: "400px",
         heading: true,
         sorting: true,
         autoload: true,
@@ -42,14 +56,14 @@ function initAccounts() {
             insertItem: function (item) {
                 return ajaxPost('insertAccount', item);
             },
-            updateItem: function (item) {
+            updateItem: function (item) { //This is a callback function
+                console.log(item);
                 return ajaxPost('insertAccount', item);
             },
             deleteItem: function (item) {
                 return ajaxPost('removeAccount', item.accountId);
             }
         },
-        
         noDataContent: "No Accounts added yet!",
         deleteConfirm: function(item) {
             confirm("The Account " + item.accountHolderName + " will be removed. Are you sure?");
@@ -58,7 +72,7 @@ function initAccounts() {
         	openAccountModal('Edit', args.item);
         },
         fields: [
-        	{name: "accountId", type: "text", visible: false},
+        	{title:'Account ID', name: "accountId", type: "text"},
         	{title: "Account holder name", name: "accountHolderName", type: "text"},
             {title: "Account Number", name: "accountNumber", type: "text"},
             {title: "IFSC code", name: "ifscCode", type: "text"},
@@ -71,9 +85,7 @@ function initAccounts() {
                     return $("<button>")
                         .attr("type", "button")
                         .attr("class", "btn btn-primary")
-                        //.attr("data-toggle", "modal")
-                        //.attr("data-target", "#accountDialog")
-                        .attr("onclick","openAccountModal();")
+                        .attr("onclick","openAccountModal('Add');")
                         .text("Add");
                 }
             }
